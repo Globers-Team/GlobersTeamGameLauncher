5677c08766b2e8d5d2ceb3b15474106ffb956333
complay11 <84672664+complay11@users.noreply.github.com>
2023-04-21 15:34:59 +0100

    Update main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
 
-
+class Game():
+    def __init__(self, id, name, link, desc):
+        self.id = id
+        self.name = name
+        self.link = link
+        self.desc = desc
+    def download(self):
+        pass
