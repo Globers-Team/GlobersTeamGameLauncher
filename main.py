2d559a5634fcd4c2ccffcebbc7fcf5cb02f282c9
complay11 <84672664+complay11@users.noreply.github.com>
2023-04-21 16:03:22 +0100

    I got downloading to work

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import requests
 
 games = []
-
+sus = "%"
 class Game():
     def __init__(self, id, name, link, desc):
         self.id = id
         self.name = name
         self.link = link
         self.desc = desc
+    def download(self):
         response = requests.get(self.link)
         open("game.zip", "wb").write(response.content)
 
 class GlobShooter(Game):
     def __init__(self):
-        super().__init__(id=0,name="Glob Shooter",link="🤓",desc="A fast-paced FPS game.")
+        super().__init__(id=0,name="Glob Shooter",link=f"https://github.com/Globers-Team/game-builds/raw/main/Glob{sus}20shooter/globusek.zip",desc="A fast-paced FPS game.")
 
 games.append(GlobShooter())
 
@@ -21,6 +22,7 @@ print("Welcome to Globers Team GameLauncher!")
 print("Here You can download all sorts of our games!")
 print("Note: You are currently using TUI/shell mode. To exit, just type: WIP")
 
+games[0].download()
 for i in games:
     print(f"{i.id + 1}. {i.name}")
     cmd = input("Enter command: ")
