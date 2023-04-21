3f7e9df0ccdf9513fb0486ca230176ce45d0604d
complay11 <84672664+complay11@users.noreply.github.com>
2023-04-21 15:41:50 +0100

    Part of Download and slight TUI changes

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ class Game():
         self.name = name
         self.link = link
         self.desc = desc
-    def download(self):
-        pass
+        response = requests.get(self.link)
+        open("game.zip", "wb").write(response.content)
 
 class GlobShooter(Game):
     def __init__(self):
         super().__init__(id=0,name="Glob Shooter",link="🤓",desc="A fast-paced FPS game.")
 
+games.append(GlobShooter())
+
 print("Welcome to Globers Team GameLauncher!")
 print("Here You can download all sorts of our games!")
 
 for i in games:
-    print(f"{games[i]}{i}")
+    print(f"{i.id + 1}. {i.name}")
